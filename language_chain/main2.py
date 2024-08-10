763c30298412e83518e47fa5fa9b7d9e87853b0d
Francis Yang <sjyti3000@gmail.com>
2024-08-11 03:21:22 +0900

    i've learn the langchian with jo-coding

---
+++ language_chain/main2.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+st.title('인공지능 시인')  # 원하는 제목 입력
+
+content = st.text_input('시를 작성할 주제를 작성해주세요!')
+
+if st.button('시 만들어보기!'):
+    with st.spinner('Wait for it....'):
+        result = chat_model.predict(content + '에 대한 시를 써줘 ')
+        st.write(result)
