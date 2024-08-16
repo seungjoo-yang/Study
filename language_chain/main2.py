0ccac2a3695821839dbb3b5416ccf6b64b3259bc
Francis Yang <sjyti3000@gmail.com>
2024-08-16 17:51:31 +0900

    update
    update

--- language_chain/main2.py
+++ language_chain/main2.py
@@ -3,20 +3,20 @@
 
 import streamlit as st
 # from langchain_community.chat_models import ChatOpenAI
-from langchain_community.llms import CTransformers
+from langchain_community.llms import ctransformers
 
 
 # chat_model = ChatOpenAI()  # 모델을 llama로 변경하기 위해 주석 처리
-llm = CTransformers(
+llm = ctransformers(
     model = 'llama-2-7b-chat.ggmlv3.q4_0.bin',
     model_type='llama'
 )
 
-st.title('달무티 화이팅')  # 원하는 제목 입력
+st.title('Make a poem for only you')  # 원하는 제목 입력
 
-content = st.text_input('시를 작성할 주제를 작성해주세요!')
+content = st.text_input('Please write the concept the poem you want to make!')
 
-if st.button('시 만들어보기!'):
+if st.button('Make a poem!'):
     with st.spinner('Wait for it....'):
-        result = llm.predict(content + '에 대한 시를 써줘 ')
+        result = llm.predict('please make a poem about the ' + content)
         st.write(result)
